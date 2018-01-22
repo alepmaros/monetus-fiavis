8a77ff805998b6bd252ed1003b24f9a3bddf57e5
dayrell <dayrell2010@gmail.com>
2018-01-22 15:31:00 -0200

    Novas dependencias, json com informações das empresas e função para retornar o status das empresas

---
+++ monetus_fiavis/fiavis/utils/get_stocks.py
@@ -0,0 +1,26 @@
+import json, requests, simplejson
+from pprint import pprint
+
+def get_stocks():
+    companies = json.load(open('stocks.json'))
+
+    for x in range(0, len(companies['stocks'])):
+        companystatus = get_company_status(companies['stocks'][x]['code'])
+        companies['stocks'][x]['status'] = companystatus
+
+    return (companies)
+
+# retorna 1 se uma ação específica retornou erro
+# retorna 2 se houve erro no retorno da API
+def get_company_status(company):
+    rsp = requests.get('https://finance.google.com/finance?q=' + company + '&output=json')
+    
+    if (rsp.status_code == 200):
+        company_data = json.loads(rsp.content[6:-2].decode('unicode_escape'))
+
+        if ('cp' in company_data):
+            return (float(company_data['cp']) / 100)
+        else:
+            return 1
+    else:
+        return 2

63b0c8ff6c3eab6fdd85e341b3c9cd3d002af37d
alepmaros <alehstk@gmail.com>
2018-01-23 12:22:49 -0200

    Calculos do fundo

--- monetus_fiavis/fiavis/views.py
+++ monetus_fiavis/fiavis/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.core import serializers
 
+from datetime import datetime
+import pytz
+
 from .models import Stock
 
 def index_fiavis(request):
 
+    time_now = datetime.now(tz=pytz.timezone("America/Sao_Paulo"))
+    stocks_today = Stock.objects.filter(time__startswith=time_now.date()).order_by('-fshare')
+
+    # valorizacao total do fundo
+    fcp = 0
+    for s in stocks_today:
+        fcp += ( s.fshare / 100.0 ) * s.vcp
+
     stocks = Stock.objects.all()
     stocks_json = serializers.serialize('json', stocks)
 
-    return render(request, 'fiavis/index.html', {'stocks': stocks_json})
+    return render(request, 'fiavis/index.html', {'fcp'         : fcp,
+                                                 'stocks_today': stocks_today,
+                                                 'stocks'      : stocks_json})

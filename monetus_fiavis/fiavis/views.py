f011263d0702fc317dc371a6d6ea10239f51ae6b
dayrell <dayrell2010@gmail.com>
2018-01-24 21:42:25 -0200

    Modificando retorno

--- monetus_fiavis/fiavis/views.py
+++ monetus_fiavis/fiavis/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.core import serializers
 
 from datetime import datetime
-import pytz
+import time
+import pytz, json
 
 from .models import Stock, Day
 
@@ -19,12 +20,12 @@ def index_fiavis(request):
     all_days_object = []
     for d in all_days:
         sd = {
-            'day': d.time,
+            'day': d.time.strftime("%Y-%m-%d %H:%M:%S"),
             'vcp': d.vcp,
-            'stocks': serializers.serialize('json', d.stock_set.all(), fields=('code', 'vcp') )
+            'stocks': json.loads(serializers.serialize('json', d.stock_set.all(), fields=('code', 'vcp') ))
         }
         all_days_object.append(sd)
-
-    return render(request, 'fiavis/index.html', {'all_days'   : all_days_object,
+        
+    return render(request, 'fiavis/index.html', {'all_days'   : json.dumps(all_days_object),
                                                  'last_stocks_updated'   : last_stocks_updated,
                                                  'last_day_updated'      : last_day_updated})

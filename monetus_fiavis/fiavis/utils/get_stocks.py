e1b526ef140cae4ca5aa8ddf996c8af388e1321a
dayrell <dayrell2010@gmail.com>
2018-03-21 22:15:33 -0300

    arrumando timezone

--- monetus_fiavis/fiavis/utils/get_stocks.py
+++ monetus_fiavis/fiavis/utils/get_stocks.py
@@ -2,6 +2,7 @@ import json, requests, simplejson, os
 from datetime import datetime, timedelta
 from alpha_vantage.timeseries import TimeSeries
 import urllib.request
+import pytz
 
 def get_all_stocks():
     f = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,11 +21,11 @@ def get_company_status(company):
     try:
         ts = TimeSeries(key=os.environ['ALPHA_KEY'])
         data, meta_data = ts.get_daily(company + '.SA')
-        today = datetime.strftime(datetime.now(), '%Y-%m-%d')
-        yesterday = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+        today = datetime.strftime(datetime.now(tz=pytz.timezone("America/Sao_Paulo"), '%Y-%m-%d')
+        yesterday = datetime.strftime(datetime.now(tz=pytz.timezone("America/Sao_Paulo") - timedelta(1), '%Y-%m-%d')
         response = {
             'valid': 0,
-            'cp': float("{0:.3f}".format(((float(data[today]['4. close']) * 100 / float(data[yesterday]['4. close'])) - 100.0))),
+            'cp': float("{0:.2f}".format(((float(data[today]['4. close']) * 100 / float(data[yesterday]['4. close'])) - 100.0))),
             'op': float(data[today]['1. open']),
             'hi': float(data[today]['2. high']),
             'lo': float(data[today]['3. low']),
